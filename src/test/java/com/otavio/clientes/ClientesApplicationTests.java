bdd9f0346bf6fa39a0037d19dfbf45b47151d801
OtavioMOliv <otaviooliveira2903@hotmail.com>
2025-09-09 17:06:23 -0300

    chore:project setup

---
+++ src/test/java/com/otavio/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otavio.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
